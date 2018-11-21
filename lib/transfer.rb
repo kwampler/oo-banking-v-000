b96543b024d5ab02a4670d9b0d2b296b14c0453f
Kate Preston <katejsenger@yahoo.com>
2018-11-21 16:51:12 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,20 @@
 class Transfer
-  # your code here
+  attr_reader :name, :transfer
+  attr_accessor :status, :transfer_amount
+
+def initialize(name)
+  @name = name
+  @transfer = transfer
+  @status = "pending"
+  @transfer_amount = transfer_amount
+end
+def valid 
+end
+def execute_transaction
+  
+end
+def reverse_transfer
+end
+
+
 end
